690c00e15e94a00036664a48e531e732c3d727be
Leomontenegro <leomontenegro6@gmail.com>
2025-08-10 09:04:00 -0300

    Added IPS patches and the required executables for the build and test process to work correctly.

---
+++ create_patches.bat
@@ -0,0 +1,5 @@
+@echo off
+.\flips.exe -c "bn3white.gba" "patchedbn3white.gba" "patches\bn3_white_titlescreen.ips"
+.\flips.exe -c "bn3blue.gba" "patchedbn3blue.gba" "patches\bn3_blue_titlescreen.ips"
+::.\flips.exe -c "bn3white.gba" "patchedbn3white.gba" "patches\bn3_jap_titlescreen.ips"
+::.\flips.exe -c "bn3blue.gba" "patchedbn3blue.gba" "patches\bn3_japblack_titlescreen.ips"
