d6e6079ebcda5c3504eb362247e4d2e71bae66b2
Leomontenegro <leomontenegro6@gmail.com>
2025-08-10 09:22:18 -0300

    Renamed patches to make it clearer which rom version they should be applied.

--- create_patches.bat
+++ create_patches.bat
@@ -1,5 +1,5 @@
 @echo off
-.\flips.exe -c "bn3white.gba" "patchedbn3white.gba" "patches\bn3_white_titlescreen.ips"
-.\flips.exe -c "bn3blue.gba" "patchedbn3blue.gba" "patches\bn3_blue_titlescreen.ips"
-::.\flips.exe -c "bn3white.gba" "patchedbn3white.gba" "patches\bn3_jap_titlescreen.ips"
-::.\flips.exe -c "bn3blue.gba" "patchedbn3blue.gba" "patches\bn3_japblack_titlescreen.ips"
+.\flips.exe -c "bn3white.gba" "patchedbn3white.gba" "patches\bn3white_white_titlescreen.ips"
+.\flips.exe -c "bn3blue.gba" "patchedbn3blue.gba" "patches\bn3blue_blue_titlescreen.ips"
+::.\flips.exe -c "bn3white.gba" "patchedbn3white.gba" "patches\bn3white_jap_titlescreen.ips"
+::.\flips.exe -c "bn3blue.gba" "patchedbn3blue.gba" "patches\bn3blue_japblack_titlescreen.ips"
